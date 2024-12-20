469c919d5a8113cab8d8cf269665b42b413f883f
nhonvo <vothuongtruongnhon2002@gmail.com>
2024-12-21 00:44:13 +0700

    comment dns

--- init.tf
+++ init.tf
@@ -69,54 +69,54 @@ resource "aws_eip" "webapi_eip" {
   instance = aws_instance.webapi_instance.id
 }
 
-# CloudFront Distribution
-resource "aws_cloudfront_distribution" "webapi_cf" {
-  origin {
-    domain_name = aws_eip.webapi_eip.public_ip # Replace with your actual domain name
-    origin_id   = "webapi-origin"
+# # CloudFront Distribution
+# resource "aws_cloudfront_distribution" "webapi_cf" {
+#   origin {
+#     domain_name = aws_eip.webapi_eip.public_ip # Replace with your actual domain name
+#     origin_id   = "webapi-origin"
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols     = ["TLSv1.2"]
-    }
-  }
+#     custom_origin_config {
+#       http_port              = 80
+#       https_port             = 443
+#       origin_protocol_policy = "http-only"
+#       origin_ssl_protocols     = ["TLSv1.2"]
+#     }
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
 
-  default_cache_behavior {
-    target_origin_id       = "webapi-origin"
-    viewer_protocol_policy = "redirect-to-https"
+#   default_cache_behavior {
+#     target_origin_id       = "webapi-origin"
+#     viewer_protocol_policy = "redirect-to-https"
 
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
+#     allowed_methods = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods  = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+#     forwarded_values {
+#       query_string = false
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
-  }
+#     min_ttl     = 0
+#     default_ttl = 3600
+#     max_ttl     = 86400
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
 
-  tags = {
-    Environment = "Demo"
-  }
-}
+#   tags = {
+#     Environment = "Demo"
+#   }
+# }
