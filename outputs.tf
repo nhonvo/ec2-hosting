34a45b9ba5658f92ae7f15e3613ee76e5c2263b0
nhonvo <vothuongtruongnhon2002@gmail.com>
2024-12-21 00:50:09 +0700

    comment dns

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
 }
 
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.webapi_cf.domain_name
-  description = "CloudFront distribution domain name"
-}
+# output "cloudfront_domain_name" {
+#   value = aws_cloudfront_distribution.webapi_cf.domain_name
+#   description = "CloudFront distribution domain name"
+# }
